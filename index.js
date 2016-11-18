0079e81db46bed190b3b6c8de0f58b28a9f20cca
Andrew Joslin <andrewtjoslin@gmail.com>
2016-11-18 14:42:06 -0700

    Respect acronyms

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict';
-module.exports = function (str) {
-	return String(str || '').toLowerCase().replace(/(?:^|\s|-)\S/g, function (m) {
-		return m.toUpperCase();
-	});
-};
+module.exports = function titleize (str) {
+  str = String(str || '')
+
+  return str.split(/\s+/)
+    .map((value) => {
+      if (value.length <= 3) return value
+
+      const hasSymbol = /[^a-zA-Z0-9]/.test(value)
+      if (hasSymbol) return value
+
+      return value.charAt(0).toUpperCase() + value.substring(1).toLowerCase()
+    })
+    .join(' ')
+}
